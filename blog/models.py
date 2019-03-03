67dc188eb70849bd9bad7716cab31c0953a0b893
christinegithub <christine13@gmail.com>
2019-03-03 17:07:29 -0500

    Topic model added

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,10 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title + " by " + self.author
+
+class Topic(models.Model):
+    name = models.CharField(max_length = 255, default = 'topic')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='topic')
+
+    def __str__(self):
+        return self.name
