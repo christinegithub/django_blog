a49af757ec3634bc0bac7c5b4f4ad6187bed0e86
christinegithub <christine13@gmail.com>
2019-03-03 16:28:08 -0500

    second article added

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from blog.models import Article
 
 
 def root(request):
     return HttpResponseRedirect('/home')
 
 def home_page(request):
-    response = render(request, 'index.html')
+    articles = Article.objects.filter(draft = False).order_by('-published_date').all()
+    context = {'articles': articles}
+    response = render(request, 'index.html', context)
     return HttpResponse(response)
